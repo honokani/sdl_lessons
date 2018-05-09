9dc0eb5f617636fe8d1dd869f91ade81b51c3cac
show <honokani.kaoriga@gmail.com>
2018-05-09 08:06:57 +0000

    ADD lesson1

---
+++ l01/ChangeLog.md
@@ -0,0 +1,3 @@
+# Changelog for l01
+
+## Unreleased changes
---
+++ l01/src/Lib.hs
@@ -0,0 +1,31 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Lib
+    ( lesson01
+    ) where
+
+import qualified SDL
+
+lesson01 :: IO ()
+lesson01 = do
+    -- create window
+    SDL.initialize []
+    sz <- pure $ SDL.V2 (fromIntegral 640) (fromIntegral 480)
+    w  <- SDL.createWindow "Lesson01!" $ SDL.defaultWindow { SDL.windowInitialSize = sz }
+
+
+    -- get screen
+    screen <- SDL.getWindowSurface w
+    -- do something
+    SDL.surfaceFillRect screen Nothing $ SDL.V4 0xFF 0xFF 0xFF 0xFF
+    SDL.updateWindowSurface w
+    SDL.delay 2000
+    SDL.surfaceFillRect screen Nothing $ SDL.V4 0x00 0x00 0x00 0x00
+    SDL.updateWindowSurface w
+    SDL.delay 2000
+
+
+    --desrtoy window
+    SDL.destroyWindow w
+    SDL.quit
+
